a442388217c02ef975298ce06c06ec5f0db379b9
Aman3849 <amanraj3849@gmail.com>
2020-08-05 11:47:01 +0530

    Course Project

--- plot2.R
+++ plot2.R
@@ -1,7 +1,11 @@
-# NEI <- readRDS("summarySCC_PM25.rds")
-# SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
 BaltNei<-subset(NEI, fips == '24510')
 BaltTotalByYear<-aggregate(Emissions~year,BaltNei,sum)
+
 png('plot2.png')
+
 barplot(height = BaltTotalByYear$Emissions,names.arg = BaltTotalByYear$year,col = BaltTotalByYear$year, xlab = "years",ylab =expression('Total PM'[2.5]* ' Emission in Tons' ) , main = expression('PM' [2.5]* ' Emission from Baltimore over the years '))
+
 dev.off()
